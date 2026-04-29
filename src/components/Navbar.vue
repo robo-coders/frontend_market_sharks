2770cd1fe1ec73be653f6c39d117a6f0cfb93741
robo-coders <62938565+robo-coders@users.noreply.github.com>
2026-04-29 08:00:16 -0700

    Fixes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,7 +23,6 @@ import {
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 
-import { ChevronsDown, Menu, LogIn, LayoutDashboard } from "lucide-vue-next";
 import ToggleTheme from "./ToggleTheme.vue";
 
 interface RouteProps {
